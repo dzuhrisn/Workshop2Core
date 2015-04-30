a702eb6485d961a8b239aed0c6b9613711e4567d
dzuhrisn <dzuhrisn@gmail.com>
2015-04-30 15:10:47 +0700

    Update Workshop2coreApplication.java

--- workshop2core/src/main/java/com/javatrainee/batch27/Workshop2coreApplication.java
+++ workshop2core/src/main/java/com/javatrainee/batch27/Workshop2coreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Workshop2coreApplication {
-
+//test edit git nyoman . by : dzuhri
     public static void main(String[] args) {
         SpringApplication.run(Workshop2coreApplication.class, args);
     }
